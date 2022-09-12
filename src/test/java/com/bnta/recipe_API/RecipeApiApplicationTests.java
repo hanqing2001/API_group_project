e1cbef4f93191b3d7b229201fbc3d17275a087c6
Hanqing Huang <h.hanqing@yahoo.com>
2022-09-12 14:40:56 +0100

    add files

---
+++ src/test/java/com/bnta/recipe_API/RecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.recipe_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
