4b6445bd2df8c1b41444208998bff61170ee3186
DaniK178 <daniellekyere@gmail.com>
2022-09-12 22:26:21 +0100

    added controller methods

--- src/main/java/com/bnta/recipe_API/controllers/IngredientController.java
+++ src/main/java/com/bnta/recipe_API/controllers/IngredientController.java
@@ -1,4 +1,36 @@
 package com.bnta.recipe_API.controllers;
 
+import com.bnta.recipe_API.models.Ingredient;
+import com.bnta.recipe_API.services.IngredientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping (value = "/ingredients")
 public class IngredientController {
+
+    @Autowired
+    IngredientService ingredientService;
+
+    @GetMapping
+    public ResponseEntity<List<Ingredient>> displayAllIngredients(){
+        List<Ingredient> ingredients = ingredientService.getAllIngredients();
+        return new ResponseEntity<>(ingredients, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Ingredient> submitNewIngredient;
+
+    @DeleteMapping
+    public
+
+//    + submitNewlngredient(): Response Entity<List<Recipe> >
+//            • displayAllingredients): ResponseEntity<List<Ingredient>>
+//+ removeAnIngredient®: ResponseEntity<String>
+
+
 }
--- src/main/java/com/bnta/recipe_API/services/IngredientService.java
+++ src/main/java/com/bnta/recipe_API/services/IngredientService.java
@@ -1,8 +1,11 @@
 package com.bnta.recipe_API.services;
 
+import com.bnta.recipe_API.models.Ingredient;
 import com.bnta.recipe_API.repositories.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class IngredientService {
@@ -10,5 +13,13 @@ public class IngredientService {
     @Autowired
     IngredientRepository ingredientRepository;
 
-    public List
+    public List<Ingredient> getAllIngredients() {
+        return ingredientRepository.findAll();
+    }
+
+    public Ingredient
+
+//    getAllIngredients: List<Ingredients>
+//+ addNewIngredient: List<Ingredients>
+//+ removeAningredient: Ingredient
 }
