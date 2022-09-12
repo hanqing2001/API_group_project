d299fb5a11c1712a1d2dbe051d2a86bbfeb059f1
sakusanpuwan <sakusanpuwan98@gmail.com>
2022-09-12 16:35:47 +0100

    added user model

--- src/main/java/com/bnta/recipe_API/models/User.java
+++ src/main/java/com/bnta/recipe_API/models/User.java
@@ -1,7 +1,39 @@
 package com.bnta.recipe_API.models;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    public User(String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public User(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
